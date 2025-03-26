f00ee6bab864f0dd39766f95b5f76d4bb87a6720
vincentthuo <vincethuo24@gmail.com>
2025-03-26 13:50:00 +0300

    Intro to Python Assignment

---
+++ Python/Assignments/IntrotoPythonAssignment.py
@@ -0,0 +1,15 @@
+#Requires the user to input a number
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+#Perform the operation based on the user's input
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2                  
+division = num1 / num2                  
+
+#Prints the results of the calculations
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
